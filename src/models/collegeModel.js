771108d47d4f6722aca5f83134aec24d3348f609
pmpatelmech <68679992+pmpatelmech@users.noreply.github.com>
2022-10-25 11:19:26 +0530

    Delete src directory

--- src/models/collegeModel.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose")
-
-const collegeModel = new mongoose.schema({
-    name: { type: String, require: "Name is required" },
-    fullName: { type: String, require: "Full name is required" }
-})
-
-module.exports.collegeModel = collegeModel
